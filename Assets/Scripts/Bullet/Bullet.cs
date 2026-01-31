3dd2c39d85a8433ebfdfdb0b62399e1546e6eada
kjdfb <919086649@qq.com>
2026-01-31 11:09:46 +0800

    按钮特效和子弹

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public float lifeTime = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+    // Update is called once per frame
+    void Update()   
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
